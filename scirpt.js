1d38c44847e1cef56afd3d450b8a3285f4f89003
23091397022 MFirmanNW <148849431+mfirmannw@users.noreply.github.com>
2024-10-29 21:35:15 +0700

    Create scirpt.js

---
+++ scirpt.js
@@ -0,0 +1,15 @@
+function filterRecipes() {
+    const input = document.getElementById('searchInput');
+    const filter = input.value.toLowerCase();
+    const container = document.getElementById('recipeContainer');
+    const cards = container.getElementsByClassName('card');
+
+    for (let i = 0; i < cards.length; i++) {
+        const recipeTitle = cards[i].getElementsByClassName('card__text')[0].getElementsByTagName('h3')[0].innerText.toLowerCase();
+        if (recipeTitle.includes(filter)) {
+            cards[i].parentElement.style.display = ""; // Show the recipe card
+        } else {
+            cards[i].parentElement.style.display = "none"; // Hide the recipe card
+        }
+    }
+}
